878abcc375b1ec7886bfb709cc362a5e55b808a8
Adnan <adnan.muhib@rocketmail.com>
2018-11-29 12:27:58 +0500

    :ok_hand: Initialized the project

---
+++ Github-Workshop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Github_Workshop.Startup))]
+namespace Github_Workshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
